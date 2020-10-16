6fa64fe8ccd783fc2b4e51b25cf4a9b967de2d85
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-15 18:52:21 -0700

    updates for the to-do-list

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # To-do-list-1
+
+This is a simple first to-do list. 
+
+Enjoy!
---
+++ javascript.js
@@ -0,0 +1,3 @@
+const clear = document.querySelector(".clear");//This selects the clear div
+const dateElement = document.getElementById("date");//This selects the date div
+const list = document.getElementById("list");
